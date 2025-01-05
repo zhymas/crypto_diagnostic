b208b8dd4c68ccabc76bb73ffabb06989774c6f5
Жимас <zhymas@MacBook-Pro-Zimas.local>
2025-01-05 19:17:14 +0200

    Implement registration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Crypto Diagnostic API",
+        default_version='v1',
+        description="API for search differents in crypto exchange",
+        terms_of_service="",
+        contact=openapi.Contact(email="shvachko319@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/')
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-schema'),
+    path('api/v1/', include('users.urls')),
 ]
